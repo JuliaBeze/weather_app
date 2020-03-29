eff66642864b118f5bd9dcd114480d3d82443eea
JuliaBeze <juliazhorova@rambler.ru>
2020-03-29 17:18:03 +0300

    project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Info from "./components/info";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Info/>
+            </div>
+        );
+    }
+}
+
+export default App;

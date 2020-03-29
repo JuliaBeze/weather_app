ee389c14310e2a8a016021e6b400d31633d79995
JuliaBeze <juliazhorova@rambler.ru>
2020-03-29 18:23:53 +0300

    weather component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import Weather from "./components/weather";
 const API_KEY = "87ecf7e957a20eabf7db0d714b7eaa99";
 
 class App extends React.Component {
+    state = {
+        tepm:undefined,
+        city:undefined,
+        country:undefined,
+        pressure:undefined,
+        sunset:undefined,
+        error:undefined
+    };
+
 
     gettingWeather = async (e) => {
         e.preventDefault();
@@ -14,15 +23,39 @@ class App extends React.Component {
         const api_url = await
             fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
         const data = await api_url.json();
-        console.log(data)
-    }
+
+        if (city){
+            let sunset = data.sys.sunset;
+            let date = new Date();
+            date.setTime(sunset); // какая дата будет отслеживаться(передаем текущую)
+            let sunset_date = date.getHours() + ":" + date.getMinutes() + ":" +date.getSeconds();
+
+            this.setState({
+                temp: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                pressure:data.main.pressure,
+                sunset:sunset_date,
+                error: ""
+            })
+        }
+
+
+    };
 
     render() {
         return (
             <div className="App">
                 <Info/>
                 <Form weatherMethod = {this.gettingWeather}/>
-                <Weather/>
+                <Weather
+                temp = {this.state.temp}
+                city = {this.state.city}
+                country = {this.state.country}
+                pressure = {this.state.pressure}
+                sunset = {this.state.sunset}
+                error = {this.state.error}
+                />
             </div>
         );
     }
--- src/components/weather.js
+++ src/components/weather.js
@@ -5,7 +5,14 @@ class Weather extends React.Component {
     render() {
         return (
             <div className="weather">
-                Погода
+                {this.props.city &&
+                <div>
+                    <p>Местоположение: {this.props.city} {this.props.country}</p>
+                    <p>Температура: {this.props.temp}</p>
+                    <p> Давление:{this.props.pressure}</p>
+                    <p>Заход солнца:{this.props.sunset}</p>
+                </div>
+                }
             </div>
         );
     }

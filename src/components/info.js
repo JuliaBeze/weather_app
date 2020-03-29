f36a61e67f2b89f9e99e0a2be833f823853ca22d
JuliaBeze <juliazhorova@rambler.ru>
2020-03-29 17:16:16 +0300

    info

---
+++ src/components/info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div className="info">
+                <h2> Погодное приложение </h2>
+                <p>Узнайтие погоду в вашем городе !</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
